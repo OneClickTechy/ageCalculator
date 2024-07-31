0308962812bb21ba295c0dc866b3f8da75e9e294
jeyapandi <jeyapandimathsgac@gmail.com>
2024-07-31 14:15:22 +0530

    half finished

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-birthDate = document.getElementById("#birthDate");
-console.log(birthDate);
+let inputBirthDate = document.querySelector("#inputBirthDate");
+let result = document.querySelector("#result");
+// let birthMonth = inputBirthDate.getMonth();
+// let birthDate = inputBirthDate.getDate();
+let calc = document.getElementById("calc");
+const currentDate = new Date().getFullYear();
+
+calc.addEventListener("click", () => {
+  console.log(currentDate);
+  console.log(new Date(inputBirthDate.value).getFullYear());
+  console.log(currentDate - new Date(inputBirthDate.value).getFullYear());
+  let age = currentDate - new Date(inputBirthDate.value).getFullYear();
+  result.textContent = `Your age is ${age}`;
+});
