d73b586a01f63726c61c04d0264e624b3f2aea32
jeyapandi <jeyapandimathsgac@gmail.com>
2024-08-01 20:01:26 +0530

    logically finished

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-  const birthDay = new Date("2002-10-04");
-  const currentDay = new Date("2024-07-13");
+const calculate = document.querySelector("#calc");
+const output = document.querySelector("#output");
+calculate.addEventListener("click", ageCalculator);
+
+function ageCalculator() {
+  const birthDay = new Date(document.querySelector("#birthDay").value);
+  const currentDay = new Date();
 
   const birthDayChild = {
     date: birthDay.getDate(),
@@ -22,15 +27,18 @@
   };
 
   //check month value and redefine the year
-  if(result.month < 0){
+  if (result.month < 0) {
     result.year--;
     result.month += 12;
   }
 
   //check date value and redefine the month
-  if(result.date < 0){
+  if (result.date < 0) {
     result.month--;
-    const totalDaysInMonth = totalDays(birthDayChild.year, birthDayChild.month -1);
+    const totalDaysInMonth = totalDays(
+      birthDayChild.year,
+      birthDayChild.month - 1
+    );
     result.date += totalDaysInMonth;
   }
   console.log(result);
@@ -38,3 +46,5 @@
   function totalDays(year, month) {
     return new Date(year, month + 1, 0).getDate();
   }
+  output.textContent = `Yout age is ${result.year} Years ${result.month} months ${result.date} days`;
+}
