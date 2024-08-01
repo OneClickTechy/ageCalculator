268c5dda711ccfe6d8b44d98c75808ead7f9ab84
jeyapandi <jeyapandimathsgac@gmail.com>
2024-08-01 17:58:52 +0530

    remove do while loop

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const birthDay = new Date("2002-05-04");
-const currentDay = new Date("2024-08-01");
+const birthDay = new Date("2002-09-04");
+const currentDay = new Date("2024-07-31");
 const monthTemplate = {
   leap: [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
   nonLeap: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
@@ -15,10 +15,14 @@ const currentDayChild = {
   year: currentDay.getFullYear(),
 };
 //variable declarations
-let ageYearCount = (ageMonthCount = ageDayCount = 0);
+let ageYearCount = 0;
+let ageMonthCount = 0;
+let ageDayCount = 0;
+let currentMonthDateBalance;
+let birthMonthDateBalance;
 
 //chk cd(m) & bd(m)
-if (currentDayChild.year - birthDayChild.year > 1) {
+if (currentDayChild.year - birthDayChild.year > 0) {
   ageYearCount =
     currentDayChild.month === birthDayChild.month &&
     currentDayChild.date === birthDayChild.date
@@ -31,13 +35,30 @@ if (currentDayChild.year - birthDayChild.year > 1) {
 // case1
 
 if (birthDayChild.month < currentDayChild.month) {
-  ageMonthCount = currentDayChild.month - birthDayChild.month - 1;
+  ageMonthCount = currentDayChild.month - birthDayChild.month - 1; //manual error(add -1 at the end of the last bracket)
+  currentMonthDateBalance = currentDayChild.date;
+  birthMonthDateBalance = chkLeap(birthDayChild.year)
+    ? monthTemplate.leap[birthDayChild.month] - birthDayChild.date
+    : monthTemplate.nonLeap[birthDayChild.month] - birthDayChild.date;
+
+  ageDayCount = currentMonthDateBalance + birthMonthDateBalance;
+} else if (birthDayChild.month > currentDayChild.month) {
+  ageMonthCount = 11 - birthDayChild.month + currentDayChild.month; //manual error(add -1 at the end of the last bracket)
+  // console.log(ageMonthCount);
+
+  currentMonthDateBalance = currentDayChild.date;
+  birthMonthDateBalance = chkLeap(birthDayChild.year)
+    ? monthTemplate.leap[birthDayChild.month] - birthDayChild.date
+    : monthTemplate.nonLeap[birthDayChild.month] - birthDayChild.date;
+
+  ageDayCount = currentMonthDateBalance + birthMonthDateBalance;
 }
 
-//tmp monthcount
-// console.log(ageMonthCount);
 //tmp yearCount
-// console.log(ageYearCount);
+console.log(ageYearCount);
+//tmp monthcount
+console.log(ageMonthCount);
+console.log(ageDayCount);
 
 //chkleap or nonleap of the year
 function chkLeap(year) {
@@ -49,5 +70,13 @@ function chkLeap(year) {
       : false
     : false;
 }
-
 // console.log(chkLeap(2002));
+
+//calculate total days in the birth month
+function totalDays(year, month) {
+  return chkLeap(year)
+    ? monthTemplate.leap[month]
+    : monthTemplate.nonLeap[month];
+}
+
+// console.log(totalDays());
