5d33ac71fcc0a9092ebc124b4d44434a5b5b16e4
jeyapandi <jeyapandimathsgac@gmail.com>
2024-08-01 08:16:33 +0530

    i need to redefine the program

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const birthDay = new Date(document.querySelector("#birthDay").value);
-const currentDay = new Date();
+const currentDay = new Date();//30.07.24
 // button selector
 const calc = document.querySelector("#calc");
 
-const birthDate = birthDay.getDate();
-const birthMonth = birthDay.getMonth();
-const birthYear = birthDay.getFullYear();
+const currentDate = currentDay.getDate();//30
+const currentMonth = currentDay.getMonth();//06 start from 0
+const currentYear = currentDay.getFullYear();//2024
 
-const currentDate = currentDay.getDate();
-const currentMonth = currentDay.getMonth();
-const currentYear = currentDay.getFullYear();
+const leapCount = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const nonLeapCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 calc.addEventListener("click", () => {
+  const birthDay = new Date(document.querySelector("#birthDay").value);//04.05.2002
+  const birthDate = birthDay.getDate();//04
+  const birthMonth = birthDay.getMonth();//04 start from 0
+  const birthYear = birthDay.getFullYear();//2002
   // console.log(birthDate, typeof birthDate);
   // console.log(birthMonth, typeof birthMonth);
   // console.log(birthYear, typeof birthYear);
@@ -18,10 +21,56 @@ calc.addEventListener("click", () => {
   // console.log(currentDate, typeof currentDate);
   // console.log(currentMonth, typeof currentMonth);
   // console.log(currentYear, typeof currentYear);
-  let ageYear = currentYear - birthYear - 1;
-  console.log(ageYear);
-  console.log(chkLeap(birthYear, currentYear));
+  let ageYear = currentYear - birthYear - 1;//2024-2002=22-1=21
+  // console.log(ageYear);
+  console.log(chkLeap(birthYear, currentYear));// false true
+  leapResult = chkLeap(birthYear, currentYear);// false true
   //calculate balance months
+  let initMonths;//undefined
+  let Months;//undefined
+  if (birthMonth <= 11) {
+    initMonths = 11 - birthMonth;//11-4=7
+  }
+  // console.log(initMonths);
+
+  //Months of the months
+  Months = currentMonth + 1;//6+1=7
+  // console.log(Months);
+
+  let balanceMonth = initMonths + Months;//7+7=14
+  // console.log(balanceMonth);
+
+  //get balance days
+  let initDays;
+  let endDays = currentDate;//30
+  if (leapResult[0] === true) {
+    // console.log(true);
+    // leapCount[birthMonth]-birthDate;
+    initDays = leapCount[birthMonth] - birthDate;//31-4=27
+  } else {
+    initDays = nonLeapCount[birthMonth] - birthDate;//31-4=27
+    // console.log(false);
+  }
+
+  let balanceDays = initDays + endDays;//27+31
+  console.log(balanceDays);//58
+
+  do {
+    if (balanceDays >= 30) {
+      balanceMonth++;
+      balanceDays -= 30;//28
+    }
+  } while (balanceDays >= 30);
+
+  do {
+    if (balanceMonth >= 12) {
+      ageYear++;
+      balanceMonth -= 12;
+    }
+  } while (balanceMonth >= 12);
+
+  console.log(ageYear, balanceMonth, balanceDays);
+  
 });
 
 function leap(year) {
@@ -38,5 +87,5 @@ function chkLeap(year1, year2) {
   const res1 = leap(year1);
   const res2 = leap(year2);
 
-  return { res1, res2 };
+  return [res1, res2];
 }
